76625c47f61c8da5a690be2123cf3aaf8fe8a68a
Daniel Pérez <daniel.perezpa@hotmail.es>
2019-06-18 21:26:36 +0200

    Refactored StudentDAOXml to simplify it

--- FileManager.DataAccess.DAO/DAOFactoriesImpl/StudentDAOXml.cs
+++ FileManager.DataAccess.DAO/DAOFactoriesImpl/StudentDAOXml.cs
@@ -1,41 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using FileManager.Common.Models;
-using FileManager.DataAccess.DAO.ReaderFactoriesImp;
-using FileManager.DataAccess.DAO.WriterFactoriesImpl;
+using FileManager.Utilities;
 
 namespace FileManager.DataAccess.DAO
 {
     public class StudentDAOXml : IAbstractStudentDAO
     { 
-        private WriterFactory writerFactoryXml = null;
-        private WriterXml writerXml = null;
-        private ReaderFactory readerFactoryXml = null;
-        private ReaderXml readerXml = null;
+        private XDocument xDocument = null;
+        public string FilePath { get; private set; }
 
         public StudentDAOXml()
         {
-            writerFactoryXml = new WriterFactory();
-            writerXml = (WriterXml)writerFactoryXml.CreateWriterXml();
-            readerFactoryXml = new ReaderFactory();
-            readerXml = (ReaderXml)readerFactoryXml.CreateReaderXml();
+            string solutionFolderPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string fileName = ConfigurationManager.AppSettings["persistence_file_xml"];
+            FilePath = solutionFolderPath + fileName;
         }
 
         public Student Add(Student student)
-        {
-            Console.WriteLine("Adding student.xml");
-            writerXml.WriteStudent(student);
+        {           
+            WriteStudent(student);
 
-            Student readStudent = readerXml.ReadStudent();
+            Student readStudent = ReadStudent();
             return readStudent;
         }
 
-        public void DeleteLastInsertedStudent()
+        private void WriteStudent(Student student)
         {
-            throw new NotImplementedException();
+            if (!File.Exists(FilePath))
+            {
+                xDocument = CreateNewDocument();
+            }
+
+            xDocument = XDocument.Load(FilePath);
+            XElement root = xDocument.Element("students");
+            XElement studentElement = CreateStudentElement(student);
+            root.Add(studentElement);
+
+            xDocument.Save(FilePath);
+        }
+
+        private XDocument CreateNewDocument()
+        {
+            XDocument xDocument = new XDocument(new XElement("students", ""));
+            xDocument.Save(FilePath);
+            return xDocument;
+        }
+
+        private XElement CreateStudentElement(Student student)
+        {
+            XElement studentElement = new XElement("student", "");
+            studentElement.Add(
+                new XElement("id", student.StudentId),
+                new XElement("name", student.Name),
+                new XElement("surname", student.Surname),
+                new XElement("dateOfBirth", DateUtilities.DateTimeToStringES(student.DateOfBirth)));
+
+            return studentElement;
+        }
+        private Student ReadStudent()
+        {
+            XElement lastStudent = ReadLastStudentFromFile();
+            List<string> fields = ReadStudentFields(lastStudent);
+            Student readStudent = CreateStudentFromFields(fields);
+            return readStudent;
+        }
+
+        private XElement ReadLastStudentFromFile()
+        {
+            XDocument doc = XDocument.Load(FilePath);
+            XElement root = doc.Root;
+            XElement lastStudent = root.Elements("student").Last();
+            return lastStudent;
+        }
+
+        private List<string> ReadStudentFields(XElement studentElement)
+        {
+            List<string> fields = new List<string>();
+            foreach (XElement e in studentElement.Elements())
+            {
+                fields.Add(e.Value);
+            }
+            return fields;
+        }
+        private Student CreateStudentFromFields(List<string> fields)
+        {
+            int id = Int32.Parse(fields[0]);
+            string name = fields[1];
+            string surname = fields[2];
+            DateTime dateOfBirth = DateUtilities.StringToDateTimeES(fields[3]);
+            Student readStudent = new Student(id, name, surname, dateOfBirth);
+            return readStudent;
         }
     }
 }
